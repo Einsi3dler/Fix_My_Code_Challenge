e61db0f7939aa7878a5a0767d0af8a5adee35d2f
Sylvester_Divine <sylvesterdivine@outlook.com>
2023-02-21 23:18:05 +0000

    ruby code complete

--- 2-sort.rb
+++ 2-sort.rb
@@ -1,22 +1,36 @@
 ###
 #
-# 
+#  Sort integer arguments (ascending) 
 #
 ###
 
 result = []
 ARGV.each do |arg|
-    # skip if not intege
+    # skip if not integer
+    next if arg !~ /^-?[0-9]+$/
+
+    # convert to integer
+    i_arg = arg.to_i
+    
+    # insert result at the right position
+    is_inserted = false
+    i = 0
+    l = result.size
     while !is_inserted && i < l do
         if result[i] < i_arg
             i += 1
         else
-            result.insert(i - 1, i_arg)
-            is_inserted = true
-            break
+            p = 0
+            result.each do |x|
+              if i_arg < x
+                result.insert(i - 1, i_arg)
+                is_inserted = true
+                break
+              end
+            end
         end
     end
     result << i_arg if !is_inserted
 end
 
-puts result
+puts result.reverse()
---
+++ test.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/ruby
+[1,2,3].each do |n|
+
+  i = 0 
+  puts "#{i}"
+  puts "#{n}!"
+  i = 1 + i
+end
